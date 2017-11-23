a540832b76b0b15462a1d6e56444c2e2d9b71461
ovicovac <ovicovac@student.tgm.ac.at>
2017-11-23 16:41:09 +0100

    openquestion implementiert (für 3 Questions)

--- src/pages/faq/faq.ts
+++ src/pages/faq/faq.ts
@@ -10,5 +10,29 @@ export class FaqPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
   
-  
+ 
+
+  openquestion1(){
+    if(document.getElementById('open1').style.display === "none") {
+      document.getElementById('open1').style.display = "block";
+    } else {
+      document.getElementById('open1').style.display = "none";
+    }
+  }
+  openquestion2(){
+    if(document.getElementById('open2').style.display === "none") {
+      document.getElementById('open2').style.display = "block";
+    } else {
+      document.getElementById('open2').style.display = "none";
+    }
+  }
+  openquestion3(){
+    if(document.getElementById('open3').style.display === "none") {
+      document.getElementById('open3').style.display = "block";
+    } else {
+      document.getElementById('open3').style.display = "none";
+    }
+  }
+
+
 }
