c03411bad634c2c482c49d011f823673120b028b
ovicovac <ovicovac@student.tgm.ac.at>
2017-12-29 00:09:09 +0100

    login basis implementiert
    vorläufige Version

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
+  
 })
 export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+  goToRegisterpage(){
+  }
+
 }
